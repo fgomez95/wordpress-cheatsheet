8b0d2b35e77b20d60ea5f3fbb173dcd159dc354b
Fernando G <kev9504@gmail.com>
2019-03-04 20:25:14 +0000

    implemented children menu

--- index.php
+++ index.php
@@ -5,7 +5,25 @@ get_header();
 //loop thru the posts
 if (have_posts()): while(have_posts()): the_post(); ?>
 
-    <a href="<?php echo get_permalink(); ?>"><h1><?php  the_title(); ?>!!!!</h1></a>
+    <?php if(has_children() OR $post->post_parent > 0){ ?>
+    
+        <span>
+            <a href="<?php echo get_permalink(get_top_ancestor_id())?>">
+                <?php echo get_the_title(get_top_ancestor_id()); ?>
+            </a>
+        </span>
+        <?php
+        
+        $args = array(
+            'child_of' => get_top_ancestor_id(),
+            'title_li' => ''
+            );
+        
+        wp_list_pages($args);
+        ?>
+    <?php } ?>
+    
+    <a href="<?php echo get_permalink(); ?>"><h1><?php  the_title(); ?></h1></a>
     
     <p><?php the_content(); ?></p>
     
--- page-portfolio.php
+++ page-portfolio.php
@@ -5,7 +5,7 @@ get_header();
 //loop thru the posts
 if (have_posts()): while(have_posts()): the_post(); ?>
 
-    <h1><?php  the_title(); ?>!!!!</h1>
+    <h1><?php  the_title(); ?></h1>
     
     <p><?php the_content(); ?></p>
     
